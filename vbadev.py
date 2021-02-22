7ebf7e37f8ede4e79360156d974d2bec641448a6
tt <tt3d@start.bg>
2021-02-22 04:11:35 +0100

    vbadev.py for fx_toolbox_v1.4.0

--- vbadev.py
+++ vbadev.py
@@ -1,8 +1,11 @@
-#In[0]
 import os 
 import re
+import xlwings as xw
 
-#In[0]
+addin_book: xw.Book = None
+addin_name = 'fx_toolbox_v1.4.0.xlsm'
+
+
 def expand_vba_declarations(vbafile: str):
     vba_types = ['.cls', '.bas']
     if os.path.splitext(vbafile)[-1] not in vba_types:
@@ -25,3 +28,26 @@ def expand_vba_declarations(vbafile: str):
     with open(vbafile, 'w+') as f:
         f.writelines(data)
 
+
+def import_modules(source_path: str, book_name: str, module_list=None):
+    macro = addin_book.macro("Import")
+    macro(source_path, book_name, module_list)
+
+
+def delete_modules(book_name: str, module_list=None):
+    macro = addin_book.macro("Delete")
+    macro(book_name, module_list)
+
+
+def export_modules(dest_path: str, book_name: str, module_list=None):
+    macro = addin_book.macro("Export")
+    macro(dest_path, book_name, module_list)
+
+
+def set_addin(addin_name: str):
+    global addin_book
+    addin_book = xw.books[addin_name]
+
+
+if __name__ == '__main__':
+    addin_book: xw.Book = xw.books[addin_name]
